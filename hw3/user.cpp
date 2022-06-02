a59db40bb08b3b953274cee4dfdcb20defbf67cf
ddanglehee <shee9812@naver.com>
2022-06-03 00:37:47 +0900

    user.cpp 주석 추가

--- hw3/user.cpp
+++ hw3/user.cpp
@@ -4,11 +4,29 @@ using namespace std;
 
 extern Product* searchedProduct;
 
+/*
+    함수 이름 : User::purchaseProduct()
+    기능      : 사용자가 상품 구매
+    전달 인자 : 없음
+    반환값    : 없음
+    생성 일자 : 2022/6/2 11:00 PM
+    작성자 :   장성희
+
+*/
 void User::purchaseProduct()
 {
     this->purchasedProductList.addProduct(*searchedProduct);
 }
 
+/*
+    함수 이름 : User::getPurchasedProductList()
+    기능      : 사용자가 구매한 상품 리스트를 반환
+    전달 인자 : 없음
+    반환값    : 사용자가 구매한 상품 리스트(ProductCollection)
+    생성 일자 : 2022/6/2 11:00 PM
+    작성자 :   장성희
+
+*/
 ProductCollection User::getPurchasedProductList()
 {
     return (this->purchasedProductList);
